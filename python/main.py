3d1d5288f6820bcacfae6505c876c57344a91600
Leevi Laune <leevilaune@Leevis-MacBook-Air.local>
2025-03-20 15:00:07 +0200

    add discord.py template and init.sh script

--- python/main.py
+++ python/main.py
@@ -1,1 +1,21 @@
-print("Hello World")
+import discord
+import os
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+print(os.getenv('DISCORD_TOKEN'))
+client.run(os.getenv('DISCORD_TOKEN'))
