b4ada35ee04ea686d06bf95dff85f12c17429880
Leevi Laune <laune.leevi@gmail.com>
2025-03-23 19:11:18 +0000

    edit run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,11 @@
-python3 python/main.py
-nohup python3 api.py > log.txt 2>&1 &
+#!/bin/bash
+
+python_folder="python"
+
+pkill -f "$python_folder/api.py"
+pkill -f "$python_folder/webserver.py"
+
+nohup python "$python_folder/api.py" > "$python_folder/api.log" 2>&1 &
+nohup python "$python_folder/webserver.py" > "$python_folder/webserver.log" 2>&1 &
+echo "Both servers are now running."
+
