f8f7b53546a10694a06bcd98dfc80c1e69c901d2
Leevi Laune <laune.leevi@gmail.com>
2025-03-23 19:28:21 +0000

    add stop.sh and edit run.sh

--- run.sh
+++ run.sh
@@ -2,10 +2,36 @@
 
 python_folder="python"
 
+# Killing old processes
 pkill -f "$python_folder/api.py"
+if [ $? -eq 0 ]; then
+    echo "$(date) Successfully killed api process"
+else
+    echo "$(date) Failed to kill api process"
+fi
+
 pkill -f "$python_folder/webserver.py"
+if [ $? -eq 0 ]; then
+    echo "$(date) Successfully killed webserver process"
+else
+    echo "$(date) Failed to kill webserver process"
+fi
 
+# Starting api.py
 nohup python "$python_folder/api.py" > "$python_folder/api.log" 2>&1 &
-nohup python "$python_folder/webserver.py" > "$python_folder/webserver.log" 2>&1 &
-echo "Both servers are now running."
+if [ $? -eq 0 ]; then
+    echo "$(date) Successfully started api, logs in $python_folder/api.log"
+else
+    echo "$(date) Failed to start api"
+fi
 
+# Starting webserver.py (without tee)
+nohup python "$python_folder/webserver.py" > "$python_folder/webserver.log" 2>&1 &
+if [ $? -eq 0 ]; then
+    echo "$(date) Successfully started webserver, logs in $python_folder/webserver.log"
+else
+    echo "$(date) Failed to start webserver"
+fi
+
+# Final confirmation
+echo "$(date) Both servers are now running."
