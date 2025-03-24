98c5ca8b37dc9db0bc57a974b36b1a4f7637aa62
Leevi Laune <leevilaune@Leevis-MacBook-Air.local>
2025-03-24 11:56:20 +0200

    remove api.py, consolidate with webserver.py

--- python/webserver.py
+++ python/webserver.py
@@ -1,10 +1,9 @@
 import os
-from flask import Flask
-from flask import render_template
-from flask_cors import CORS
+from flask import Flask, request, jsonify,render_template
+from backend.auth import Auth
 
 app = Flask(__name__)
-CORS(app)
+auth_client = Auth()
 @app.route("/")
 def hello_world():
     backend_url = os.getenv('BACKEND_URL','http://80.220.204.247:3000')
@@ -13,5 +12,17 @@ def hello_world():
 @app.route("/user/<user>")
 def user(user: str):
     return render_template("user.html",user=user)
+
+
+@app.route("/auth", methods=["POST","GET"])
+def auth():
+    data = request.get_json()["data"]
+    print(data)
+    if not data:
+        return jsonify({"error": "Invalid or no JSON data received"}), 400
+
+    username = data.get('name')
+    password = data.get('password')
+    return jsonify(auth_client.authenticate(username, password))
 
 app.run(debug=True, host='0.0.0.0', port=80)
