363d85106dd01e34a12f361df5143968e7340f13
Leevi Laune <leevilaune@MacBookAir.home>
2025-04-03 21:18:35 +0300

    add chat features and start work on multiuser chat

--- python/static/js/chat.js
+++ python/static/js/chat.js
@@ -1,4 +1,58 @@
 
-function sendMessage() {
+
+
+async function sendMessage() {
+    let input = document.getElementById("userInput");
+    let message = input.value.trim();
+    if (message === "") return;
+    let chatBox = document.getElementById("chatBox");
+
+    const user = document.getElementById("userName").firstChild.nodeValue;
+    console.log(user);
+
+    console.log(message);
+    const msg = {
+        "message": message,
+        "user": user,
+        "time": new Date().getTime(),
+    }
+    try {
+        const response = await fetch("/api/message/send", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(msg)
+        })
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        let userMessage = document.createElement("div");
+        userMessage.classList.add("chat-message", "user-message");
+        userMessage.innerHTML = `<div class="message-header">${user}</div><div class="message-content">${message}</div>`;
+        chatBox.appendChild(userMessage);
+    } catch (err) {
+        console.log(err);
+    }
+}
+async function updateMessages() {
+    let input = document.getElementById("userInput");
+    let message = input.value.trim();
+    if (message === "") return;
+    let chatBox = document.getElementById("chatBox");
+    const user = document.getElementById("userName").firstChild.nodeValue;
+    console.log(user);
 
 }
+async function getMessages() {
+    const messages = await fetch(`/api/messages`, {
+        method: "GET"
+    })
+    .then((response) => response.json())
+}
+setInterval(() => {
+    fetch("http://your-api-endpoint.com")
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(error => console.error("Error:", error));
+}, 1000);
